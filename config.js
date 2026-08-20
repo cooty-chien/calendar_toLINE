4cb0ae89a61a05512afef2792396c44923ccc2e5
cooty <cooty09312@gmail.com>
2026-08-20 16:28:54 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+// ===== config.js =====
+// 前端環境設定
+// 只需要修改這裡，不需要修改 app.js
+
+const CONFIG = {
+  // LINE LIFF ID
+  LIFF_ID: '你的日曆 LIFF_ID',
+
+  // GAS Web App API URL
+  GAS_URL: 'https://script.google.com/macros/s/你的GAS網址/exec'
+};
